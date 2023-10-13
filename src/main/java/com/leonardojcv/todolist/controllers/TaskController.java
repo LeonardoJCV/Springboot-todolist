2e6509b9fc20576959e8b94733af6e1c64140f05
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-13 01:11:11 -0300

    LocalDateTime validation and returning tasks per idUser

--- src/main/java/com/leonardojcv/todolist/controllers/TaskController.java
+++ src/main/java/com/leonardojcv/todolist/controllers/TaskController.java
@@ -1,8 +1,13 @@
 package com.leonardojcv.todolist.controllers;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +26,28 @@ public class TaskController {
 	private TaskRepository taskRepository;
 	
 	@PostMapping
-	public Task create(@RequestBody Task task, HttpServletRequest request) {
+	public ResponseEntity create(@RequestBody Task task, HttpServletRequest request) {
 		// Recuperação do parametro user.getId() no contrller (idUser)
 		task.setIdUser((UUID)request.getAttribute("idUser"));
+		// Validação da data
+		// Se a data atual for maior que a data de incio da tarefa, não pode realizar a inserção
+		// Ou se a data atual for depois da data de finalização da tarefa
+		if (LocalDateTime.now().isAfter(task.getStartAt()) || LocalDateTime.now().isAfter(task.getEndAt())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("A data de início / data de termino deve ser maior que a data atual");
+		}
 		
-		return this.taskRepository.save(task);
+		if (task.getStartAt().isAfter(task.getEndAt())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("A data de início deve ser  menor do que a data de termino");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(task));
+	}
+	
+	// Retorna uma lista de tudo o que for relacionado ao usuario correspondente
+	@GetMapping
+	public List<Task> list(HttpServletRequest request) {
+		return this.taskRepository.findByIdUser((UUID)request.getAttribute("idUser"));
 	}
 }
--- src/main/java/com/leonardojcv/todolist/repositories/TaskRepository.java
+++ src/main/java/com/leonardojcv/todolist/repositories/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.leonardojcv.todolist.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.leonardojcv.todolist.entities.Task;
 
 public interface TaskRepository extends JpaRepository<Task, UUID>{
-
+	List<Task>findByIdUser(UUID idUser);
 }
