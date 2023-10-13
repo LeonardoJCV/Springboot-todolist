821b1f6d0671a8d001c969e2b30cc4f0cb7dae80
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-13 00:45:38 -0300

    adding servletPath and idUser request

--- src/main/java/com/leonardojcv/todolist/controllers/TaskController.java
+++ src/main/java/com/leonardojcv/todolist/controllers/TaskController.java
@@ -1,5 +1,7 @@
 package com.leonardojcv.todolist.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.leonardojcv.todolist.entities.Task;
 import com.leonardojcv.todolist.repositories.TaskRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -17,8 +21,10 @@ public class TaskController {
 	private TaskRepository taskRepository;
 	
 	@PostMapping
-	public Task create(@RequestBody Task task) {
+	public Task create(@RequestBody Task task, HttpServletRequest request) {
+		// Recuperação do parametro user.getId() no contrller (idUser)
+		task.setIdUser((UUID)request.getAttribute("idUser"));
+		
 		return this.taskRepository.save(task);
 	}
-	
 }
--- src/main/java/com/leonardojcv/todolist/filters/FilterTaskAuth.java
+++ src/main/java/com/leonardojcv/todolist/filters/FilterTaskAuth.java
@@ -16,41 +16,51 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth extends OncePerRequestFilter{
-	
+public class FilterTaskAuth extends OncePerRequestFilter {
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-		throws ServletException, IOException {
-			//Pega a autenticação (Usuario e senha)
+			throws ServletException, IOException {
+		var servletPath = request.getServletPath();
+		if (servletPath.equals("/tasks")) {
+			// Pega a autenticação (Usuario e senha)
 			var authorization = request.getHeader("Authorization");
-			//Como a authorization gera a palavra "Basic" é necessário remove-la através do substring().
+			// Como a authorization gera a palavra "Basic" é necessário remove-la através do
+			// substring().
 			var authEncoded = authorization.substring("Basic".length()).trim();
-			//Decode do base64
-			byte[]authDecoded = Base64.getDecoder().decode(authEncoded);
-			//Conversão do array de bytes para String
+			// Decode do base64
+			byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+			// Conversão do array de bytes para String
 			var authString = new String(authDecoded);
-			//A String é gerada da seguinte forma usuario:senha, então é necessario o split (em array para acessar as posições).
+			// A String é gerada da seguinte forma usuario:senha, então é necessario o split
+			// (em array para acessar as posições).
 			String[] credentials = authString.split(":");
 			String username = credentials[0];
 			String password = credentials[1];
-			
-			//Valida usúario
+
+			// Valida usúario
 			var user = this.userRepository.findByUsername(username);
 			if (user == null) {
 				response.sendError(401);
-			}
-			else {
-				//Valida senha
+			} else {
+				// Valida senha
 				var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 				if (passwordVerify.verified) {
-					//Segue viagem
+					// Atributo "idUser" com o valor do user.getId() para envio do parametro para o controller.
+					request.setAttribute("idUser", user.getId());
 					filterChain.doFilter(request, response);
 				} else {
 					response.sendError(401);
 				}
-			}	
+
+			}
+		}
+		// Segue viagem
+		else {
+			filterChain.doFilter(request, response);
+		}
 	}
 }
